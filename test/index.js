04d606c95fdf88016075a36c433c7d02c05c1c7c
supersimple <toddresudek@gmail.com>
2016-04-27 22:25:17 -0600

    adding mocha test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 var should = require('chai').should(),
     disallowed = require('../index'),
     disallow = disallowed.disallow,
-    setup_disallow = disallowed.set_disallowed_words;
+    setup_disallow = disallowed.set_disallowed_words(['foo', 'bar']);
 
 describe('#disallow', function() {
-  // it('strips disallowed words from string', function() {
-  //   disallow('').should.equal('');
-  // });
+  it('strips disallowed words from string', function() {
+    disallow('foo and bar and baz is test.').should.equal(' and  and baz is test.');
+  });
 });
